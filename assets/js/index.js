e99d7a49eb226e54da40c97bff914690c0512c3b
LgCookie <lgc2333@126.com>
2025-04-20 04:08:58 +0800

    up

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,40 +1,40 @@
-let bgUrl = null;
-let changing = false;
+let bgUrl = null
+let changing = false
 
 function sleep(time) {
   return new Promise((resolve) => {
-    setTimeout(resolve, time);
-  });
+    setTimeout(resolve, time)
+  })
 }
 
 async function fetchBG() {
-  // 原api没法过cors 我用cf workers反代了
+  // lolicon api 很好的反 cloudflare，使我迅速红温
+  // 不想处理了
   const params = new URLSearchParams({
     num: 10,
-    proxy: 'fuck-cors.lgc2333.top',
     tag: '萝莉|少女',
     excludeAI: 1,
-  });
-  const apiUrl = `https://fuck-cors.lgc2333.top/setu/v2?${params}`;
+  })
+  const apiUrl = `https://fuck-cors.lgc2333.top/setu/v2?${params}`
   const res = await fetch(apiUrl, {
     headers: { 'upstream-host': 'api.lolicon.app' },
-  });
-  let { data } = await res.json();
+  })
+  let { data } = await res.json()
   // console.log(data);
 
   // 屏幕比例小于3：4用竖屏图，否则横屏
   // 如果没有筛选到指定尺寸的图，那么直接用返回的第一个图
-  const { innerWidth, innerHeight } = window;
-  const isPortrait = innerWidth / innerHeight <= 3 / 4;
+  const { innerWidth, innerHeight } = window
+  const isPortrait = innerWidth / innerHeight <= 3 / 4
   const filteredData = data.filter((v) => {
-    const { width, height } = v;
-    let result = width >= height; // 是否横屏
-    if (isPortrait) result = !result;
-    return result;
-  });
-  if (filteredData.length > 0) data = filteredData;
-  const selectedPic = data[0];
-  const picUrl = selectedPic.urls.original;
+    const { width, height } = v
+    let result = width >= height // 是否横屏
+    if (isPortrait) result = !result
+    return result
+  })
+  if (filteredData.length > 0) data = filteredData
+  const selectedPic = data[0]
+  const picUrl = selectedPic.urls.original
 
   // get pic
   const picRes = await fetch(picUrl, {
@@ -42,59 +42,59 @@ async function fetchBG() {
       'upstream-host': 'i.pximg.net',
       'real-referer': 'https://www.pixiv.net/',
     },
-  });
-  const imgType = picRes.headers.get('content-type');
+  })
+  const imgType = picRes.headers.get('content-type')
   if (!imgType || !imgType.includes('image'))
-    throw new TypeError(`返回数据类型不对 (${imgType})`);
-  return [selectedPic, await picRes.blob()];
+    throw new TypeError(`返回数据类型不对 (${imgType})`)
+  return [selectedPic, await picRes.blob()]
 }
 
 async function changeBG() {
   /** @type {HTMLAnchorElement} */
-  const bgInfoA = document.getElementById('bg-info');
+  const bgInfoA = document.getElementById('bg-info')
   /** @type {HTMLAnchorElement} */
-  const changeElem = document.getElementById('change');
+  const changeElem = document.getElementById('change')
 
   const task = async () => {
-    if (bgUrl) URL.revokeObjectURL(bgUrl);
+    if (bgUrl) URL.revokeObjectURL(bgUrl)
 
-    const [bgInfo, bg] = await fetchBG();
-    bgUrl = URL.createObjectURL(bg);
+    const [bgInfo, bg] = await fetchBG()
+    bgUrl = URL.createObjectURL(bg)
 
     // animation
-    const bgElement = document.getElementById('bg');
-    bgElement.style.animation = `bg-fade-out 1s cubic-bezier(0, 0, 0.2, 1)`;
+    const bgElement = document.getElementById('bg')
+    bgElement.style.animation = `bg-fade-out 1s cubic-bezier(0, 0, 0.2, 1)`
 
     // wait animation
     await new Promise((resolve) => {
-      bgElement.addEventListener('animationend', resolve, { once: true });
-    });
+      bgElement.addEventListener('animationend', resolve, { once: true })
+    })
 
-    bgElement.style.backgroundImage = `url("${bgUrl}")`;
-    bgElement.style.animation = `bg-fade-in 1s cubic-bezier(0, 0, 0.2, 1)`;
+    bgElement.style.backgroundImage = `url("${bgUrl}")`
+    bgElement.style.animation = `bg-fade-in 1s cubic-bezier(0, 0, 0.2, 1)`
 
     // show pic info
-    bgInfoA.innerText = bgInfo.title;
-    bgInfoA.href = `https://www.pixiv.net/artworks/${bgInfo.pid}`;
-  };
-
-  if (changing) return;
-  changing = true;
-
-  // eslint-disable-next-line no-script-url
-  changeElem.href = 'javascript:void(0)';
-  // bgInfoA.href = '#';
-  bgInfoA.innerText = 'Loading~';
-
-  try {
-    await task();
-  } catch (e) {
-    console.log(e);
-    await sleep(3 * 1000);
-    await changeBG();
+    bgInfoA.innerText = bgInfo.title
+    bgInfoA.href = `https://www.pixiv.net/artworks/${bgInfo.pid}`
   }
 
-  changing = false;
+  if (changing) return
+  changing = true
+
   // eslint-disable-next-line no-script-url
-  changeElem.href = 'javascript:changeBG()';
+  changeElem.href = 'javascript:void(0)'
+  // bgInfoA.href = '#';
+  bgInfoA.innerText = 'Loading~'
+
+  try {
+    await task()
+  } catch (e) {
+    console.log(e)
+    await sleep(3 * 1000)
+    await changeBG()
+  }
+
+  changing = false
+  // eslint-disable-next-line no-script-url
+  changeElem.href = 'javascript:changeBG()'
 }
--- assets/js/roll.js
+++ assets/js/roll.js
@@ -6,14 +6,14 @@ const LINKS = [
   'https://www.bilibili.com/video/BV1SD4y1J7uY/',
   'https://www.bilibili.com/video/BV13m4y1D7kT/',
   'https://www.bilibili.com/video/BV1iJ411J7f5/',
-];
+]
 
 /**
  * @param {number} max
  * @returns {number}
  */
 function randMax(max) {
-  return Math.floor(Math.random() * (max + 1));
+  return Math.floor(Math.random() * (max + 1))
 }
 
 /**
@@ -21,23 +21,23 @@ function randMax(max) {
  * @returns {string}
  */
 function choose(list) {
-  const index = randMax(list.length - 1);
-  return list[index];
+  const index = randMax(list.length - 1)
+  return list[index]
 }
 
 function roll() {
-  const date = new Date();
+  const date = new Date()
   if (!(date.getMonth() === 3 && date.getDate() === 1)) {
-    localStorage.removeItem('rolled');
-    return;
+    localStorage.removeItem('rolled')
+    return
   }
 
   // 第一次必定触发，之后 50% 概率
   if (!localStorage.getItem('rolled') || randMax(99) >= 50) {
-    localStorage.setItem('rolled', '1');
-    const link = choose(LINKS);
-    window.location.href = link;
+    localStorage.setItem('rolled', '1')
+    const link = choose(LINKS)
+    window.location.href = link
   }
 }
 
-roll();
+roll()
